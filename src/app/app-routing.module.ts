668376e269c0b9d25cf3c2dc3328ad702ffdbf29
Nasserallah-hour <nasserallah.hourichi@gmail.com>
2023-01-15 13:44:22 +0100

    Added assets before changing the home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './modules/general/about/about.component';
+import { ContactComponent } from './modules/general/contact/contact.component';
+import { CurriculemComponent } from './modules/general/curriculem/curriculem.component';
+import { HomeComponent } from './modules/general/home/home.component';
+import { NotFoundComponent } from './modules/general/not-found/not-found.component';
+import { SigninComponent } from './modules/general/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'cv', component: CurriculemComponent },
+  { path: '**', component: NotFoundComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './modules/general/about/about.component';
+import { ContactComponent } from './modules/general/contact/contact.component';
+import { HomeComponent } from './modules/general/home/home.component';
+import { NotFoundComponent } from './modules/general/not-found/not-found.component';
+import { SigninComponent } from './modules/general/signin/signin.component';
+import { ProduitComponent } from './modules/general/produit/produit.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CurriculemComponent } from './modules/general/curriculem/curriculem.component';
+import { CustomCardComponent } from './modules/Widgets/custom-card/custom-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    NotFoundComponent,
+    SigninComponent,
+    AboutComponent,
+    ProduitComponent,
+    CurriculemComponent,
+    CustomCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
